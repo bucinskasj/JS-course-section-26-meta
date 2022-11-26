feb16ca67bc3e66424ee86d3775cb842964ffee7
Jonas Bucinskas <jonas@bucinskas.lt>
2022-11-26 13:48:12 +0200

    Lesson 470

---
+++ app.js
@@ -0,0 +1,23 @@
+//Library land
+
+const uid = Symbol('uid');
+console.log(uid);
+
+const user = {
+  // id: 'p1',
+  [uid]: 'p1',
+  name: 'Max',
+  age: 30
+};
+
+user[uid] = 'p3';
+
+//App land => Using the library
+
+user.id = 'p2'; //this should not be possible
+
+console.log(user[Symbol('uid')]);
+
+console.log(Symbol('uid') === Symbol('uid'));
+
+console.log(user);
